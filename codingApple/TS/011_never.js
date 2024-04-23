c18f93342de480fb319c6cfc54be71f2bef13376
jsunbin <jsnb6n@gmail.com>
2024-04-23 23:58:01 +0900

    코딩애플 - 함수에서 사용하는 never 타입

---
+++ codingApple/TS/011_never.js
@@ -0,0 +1,22 @@
+// 함수에서 사용하는 never ==========
+function 함수() {
+    // 조건1. 리턴이 없어야함.
+    // 조건2. endpoint가 없어야함. = 끝나지 않는 함수여야함.
+    throw new Error("");
+    while (true) {
+        // 내부코드를 무한히 반복함
+    }
+}
+// never 타입의 활용 =====
+function 함수2(parameter) {
+    if (typeof parameter === "string") {
+        console.log(parameter);
+    }
+    else {
+        console.log(parameter);
+    }
+}
+var 함수3 = function () {
+    throw new Error("");
+};
+export {};
